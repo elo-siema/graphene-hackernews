141e4836c780c5f64e0076aade2258818c38b8ee
Michał Flak <mflak@monadical.com>
2022-02-16 17:10:05 +0100

    Finished course

---
+++ hackernews/hackernews/utils.py
@@ -0,0 +1,5 @@
+from hackernews import settings
+class DisableCSRF(MiddlewareMixin):
+   def process_request(self, request):
+      if settings.DEBUG:
+         setattr(request, '_dont_enforce_csrf_checks', True)
